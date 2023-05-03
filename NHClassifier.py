ba97dfa2ecec604e78ce2b80c669776367178c4a
NajiyaMujeeb <128771892+NajiyaMujeeb@users.noreply.github.com>
2023-05-03 17:16:02 +0530

    Update NHClassifier.py

--- NHClassifier.py
+++ NHClassifier.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
-bbc_text = pd.read_csv(r"C:\Users\Sreya\Desktop\duk notes\semester 2\nlpir\bbc-text.txt")
+bbc_text = pd.read_csv(r"bbc-text (1).txt")
 bbc_text=bbc_text.rename(columns = {'text': 'News_Headline'}, inplace = False)
 bbc_text.category = bbc_text.category.map({'tech':0, 'business':1, 'sport':2, 'entertainment':3, 'politics':4})
 
@@ -21,7 +21,7 @@ naivebayes = MultinomialNB()
 naivebayes.fit(X_transformed, y_train)
 saved_model = pickle.dumps(naivebayes)
 s = pickle.loads(saved_model)
-st.title('News Headline Classifier')
+st.title('Text Classifier')
 input = st.text_area("Please enter the news headline", value="")
 if st.button("Predict"):
     v=vector.transform([input]).toarray()
